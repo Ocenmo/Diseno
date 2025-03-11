aeb643ac0b86ce5c1ad26bc685f073ad9bedbd8f
Ocenmo <oemorales@uninorte.edu.co>
2025-03-11 12:37:54 -0500

    .

--- P1_S4/client/viatracker/src/App.jsx
+++ P1_S4/client/viatracker/src/App.jsx
@@ -4,7 +4,7 @@ import Table from "../components/Table";
 import { connectWebSocket } from "./WebsocketService"; // Importamos el servicio
 
 function App() {
-    const [data, setData] = useState([]);
+    const [data, seData] = useState([]);
     const [error, setError] = useState(null);
 
     useEffect(() => {
