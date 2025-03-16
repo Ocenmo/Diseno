60d6e39d6bb125c7558e38986e40b29c913d9e56
Ocenmo <oemorales@uninorte.edu.co>
2025-03-16 13:15:08 -0500

    Actualizar clave API de Google Maps a variable de entorno

--- P1_S4/client/viatracker/components/Mapa.jsx
+++ P1_S4/client/viatracker/components/Mapa.jsx
@@ -3,7 +3,7 @@ import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 const defaultPosition = { lat: 37.7749, lng: -122.4194 };
 const Map = ({ latitude = defaultPosition.lat, longitude = defaultPosition.lng }) => {
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyB5em391M0eCTbKlaH610KzWPno8rBiMPc", // Reemplaza con tu clave API
+        googleMapsApiKey: import.meta.env.VITE_MAP_KEY, // Reemplaza con tu clave API
     });
 
     if (!isLoaded) return <p>Cargando mapa...</p>;
