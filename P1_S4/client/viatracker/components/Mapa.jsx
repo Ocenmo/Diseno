468c0ace37a1f01bd0461230aa1158ce2959eef5
Ocenmo <oemorales@uninorte.edu.co>
2025-03-16 11:43:00 -0500

    Agregar funcionalidad de mapa en tiempo real y manejo de WebSocket en script.js; actualizar clave API en Mapa.jsx

--- P1_S4/client/viatracker/components/Mapa.jsx
+++ P1_S4/client/viatracker/components/Mapa.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const defaultPosition = { lat: 37.7749, lng: -122.4194 };
 const Map = ({ latitude = defaultPosition.lat, longitude = defaultPosition.lng }) => {
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyB5em391M0eCTbKlaH610KzWPno8rBiMPc", // Reemplaza con tu clave API
+        googleMapsApiKey: process.env.MAP_KEY, // Reemplaza con tu clave API
     });
 
     if (!isLoaded) return <p>Cargando mapa...</p>;
