5729937c877bd1f37b4cb8dd67e44d1775bc9ddb
Ocenmo <oemorales@uninorte.edu.co>
2025-03-19 16:13:07 -0500

    Eliminar la funcionalidad de trazar el recorrido en el mapa y simplificar la inicialización de la posición predeterminada

--- P1_S4/client/viatracker/src/components/Mapa.jsx
+++ P1_S4/client/viatracker/src/components/Mapa.jsx
@@ -1,32 +1,14 @@
-import { GoogleMap, Marker, Polyline, useLoadScript } from "@react-google-maps/api";
-import { useEffect, useState } from "react";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { latestLocation } from "../services/api";
 
+const latestData = latestLocation();
+const defaultPosition = { lat: latestData.Latitud, lng: latestData.Longitud };
 const ApiKey = import.meta.env.VITE_API_KEY;
 
-const Map = ({ latitude, longitude, path }) => {
+const Map = ({ latitude, longitude }) => {
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: ApiKey,
     });
-
-    const [defaultPosition, setDefaultPosition] = useState({ lat: 0, lng: 0 });
-
-    useEffect(() => {
-        const fetchLatestLocation = async () => {
-            try {
-                const latestData = await latestLocation();
-                if (latestData && latestData.Latitud && latestData.Longitud) {
-                    setDefaultPosition({
-                        lat: latestData.Latitud,
-                        lng: latestData.Longitud,
-                    });
-                }
-            } catch (error) {
-                console.error("Error fetching latest location:", error);
-            }
-        };
-        fetchLatestLocation();
-    }, []);
 
     if (!isLoaded) return <p>Cargando mapa...</p>;
 
@@ -39,20 +21,7 @@ const Map = ({ latitude, longitude, path }) => {
             center={{ lat: validLat, lng: validLng }}
             mapContainerStyle={{ width: "100%", height: "500px" }}
         >
-            {/* Marcador en la última ubicación */}
             <Marker position={{ lat: validLat, lng: validLng }} />
-
-            {/* Línea del recorrido */}
-            {path.length > 1 && (
-                <Polyline
-                    path={path.map((coord) => ({ lat: coord[0], lng: coord[1] }))}
-                    options={{
-                        strokeColor: "#FF0000",
-                        strokeOpacity: 0.8,
-                        strokeWeight: 4,
-                    }}
-                />
-            )}
         </GoogleMap>
     );
 };
