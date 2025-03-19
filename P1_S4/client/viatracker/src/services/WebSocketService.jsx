0070ea1a2bf11d50a25feb5440eb54b5bd61e9f0
Ocenmo <oemorales@uninorte.edu.co>
2025-03-19 09:41:26 -0500

    Implementar reconexión automática en el servicio WebSocket y mejorar los registros de conexión

--- P1_S4/client/viatracker/src/services/WebSocketService.jsx
+++ P1_S4/client/viatracker/src/services/WebSocketService.jsx
@@ -1,24 +1,37 @@
 export const connectWebSocket = (setDataCallback) => {
     const wsUrl = import.meta.env.VITE_WS_URL;
-    console.log("Conectando a WebSocket en", wsUrl);
-    const ws = new WebSocket(wsUrl);
+    let reconnectInterval = 5000;
+    let ws;
 
-    ws.onmessage = (event) => {
-        try {
-            const newData = JSON.parse(event.data);
-            console.log("Nuevo dato recibido:", newData);
+    const connect = () => {
+        console.log("Conectando a WebSocket en", wsUrl);
+        const ws = new WebSocket(wsUrl);
 
-            if (isValidCoordinate(newData.latitude, newData.longitude)) {
-                setDataCallback(newData);
+        ws.onopen = () => {
+            console.log("Conexión WebSocket establecida");
+        };
+
+        ws.onmessage = (event) => {
+            try {
+                const newData = JSON.parse(event.data);
+                console.log("Nuevo dato recibido:", newData);
+
+                if (isValidCoordinate(newData.latitude, newData.longitude)) {
+                    setDataCallback(newData);
+                }
+            } catch (err) {
+                console.error("Error procesando mensaje WebSocket:", err);
             }
-        } catch (err) {
-            console.error("Error procesando mensaje WebSocket:", err);
-        }
+        };
+
+        ws.onerror = (err) => console.error("Error en WebSocket:", err);
+        ws.onclose = () => {
+            console.warn("Conexión WebSocket cerrada, reconectando en", reconnectInterval, "ms");
+            setTimeout(connect, reconnectInterval);
+
+        };
     };
-
-    ws.onerror = (err) => console.error("Error en WebSocket:", err);
-    ws.onclose = () => console.warn("Conexión WebSocket cerrada");
-
+    connect();
     return ws;
 };
 
