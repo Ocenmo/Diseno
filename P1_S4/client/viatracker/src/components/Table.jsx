5535ffa46147cd3ba8f1303fcea6dad7936dde6f
Ocenmo <oemorales@uninorte.edu.co>
2025-03-23 11:10:34 -0500

    Agregar contenedor scrollable a la tabla y mejorar la visibilidad de los encabezados al hacer scroll

--- P1_S4/client/viatracker/src/components/Table.jsx
+++ P1_S4/client/viatracker/src/components/Table.jsx
@@ -1,32 +1,34 @@
 import React from "react";
-import "./Table.css"
+import "./Table.css";
 
 const Table = ({ data, error }) => {
     return (
-        <div>
+        <div className="table-container">
             {error ? (
                 <p style={{ color: "red" }}>{error}</p>
             ) : data.length > 0 ? (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Latitud</th>
-                            <th>Longitud</th>
-                            <th>Timestamp</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.id}</td>
-                                <td>{item.latitude}</td>
-                                <td>{item.longitude}</td>
-                                <td>{item.timestamp}</td>
+                <div className="scrollable-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Latitud</th>
+                                <th>Longitud</th>
+                                <th>Timestamp</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {data.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.id}</td>
+                                    <td>{item.Latitud}</td>  {/* Corregido */}
+                                    <td>{item.Longitud}</td> {/* Corregido */}
+                                    <td>{item.TimeStamp}</td> {/* Corregido */}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             ) : (
                 <p>Cargando datos...</p>
             )}
