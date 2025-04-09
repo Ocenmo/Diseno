994f7b742a32c383b56317e7d28f58cfb9c65204
Ocenmo <oemorales@uninorte.edu.co>
2025-04-09 13:35:15 -0500

    Añadir manejadores de eventos para centrar el mapa y restablecer el índice de la ruta en el componente Rutas.

--- P1_S4/client/viatracker/src/pages/Rutas.jsx
+++ P1_S4/client/viatracker/src/pages/Rutas.jsx
@@ -52,6 +52,13 @@ const Rutas = () => {
                 key={mapKey}
                 zoom={15}
                 center={path.length > 0 ? path[0] : { lat: 11.020082, lng: -74.850364 }}
+                onCenterChanged={() => {
+                    if (path.length > 0) {
+                        const center = path[currentIndex];
+                        setPath([center]);
+                    }
+                }}
+                onClick={() => setCurrentIndex(0)}
                 mapContainerStyle={{ width: "100%", height: "calc(100vh - 60px)" }}
             >
                 <div className="flex items-center justify-center md:w-1/3">
